c81e46d640b7f92db226c7a395aa70d7fcf7eb5b
ReyraGaming <Reynaldijones@gmail.com>
2024-01-13 15:43:06 +0800

    tetap gk bisa eh

--- src/app/components/card.jsx
+++ src/app/components/card.jsx
@@ -1,15 +1,53 @@
-import { games } from "../../../public/games";
-import Image from 'next/link'
+'use client'
 
-const Card = () => {
-    const cardList = games.map((game) => (
-        <div className="flex flex-col gap-3 rounded-lg font-fireSans" key={game.title}>
-            <p>{game.genre}</p>
-            <h2>{game.title}</h2>
-            <p>{game.price}</p>
-            <p>{game.discount}</p>
-        </div>
-    ))
-}
+import Image from "next/link";
+import React from "react";
 
-export default Card;
+export default function Card() {
+  const games = [
+    {
+      id: 1,
+      image: "joko",
+      title: "telo",
+      genre: "lol",
+      price: "Rp. 16.000",
+      discount: "Rp. 14.000",
+    },
+    {
+      id: 2,
+      image: "joko",
+      title: "telo",
+      genre: "lol",
+      price: "Rp. 16.000",
+      discount: "Rp. 14.000",
+    },
+    {
+      id: 3,
+      image: "joko",
+      title: "telo",
+      genre: "lol",
+      price: "Rp. 16.000",
+      discount: "Rp. 14.000",
+    },
+    {
+      id: 4,
+      image: "joko",
+      title: "telo",
+      genre: "lol",
+      price: "Rp. 16.000",
+      discount: "Rp. 14.000",
+    },
+  ];
+
+  const cardList = games.map((game) => (
+    <div
+      className="flex flex-col gap-3 rounded-lg font-fireSans"
+      key={game.id}
+    >
+      <p>{game.genre}</p>
+      <h2>{game.title}</h2>
+      <p>{game.price}</p>
+      <p>{game.discount}</p>
+    </div>
+  ));
+};
