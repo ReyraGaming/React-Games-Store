a17aa90e9ae7fdcee15bf2f53f6e758fbaf28032
ReyraGaming <Reynaldijones@gmail.com>
2024-02-23 19:44:09 +0800

    Update

--- src/app/components/populars.jsx
+++ src/app/components/populars.jsx
@@ -5,7 +5,7 @@ import Data from "../data/populars";
 
 export default function Populars() {
   return (
-    <div className="flex flex-row gap-6 py-3">
+    <div className="flex flex-col md:flex-row gap-6 py-3">
       {Data.map((item, index) => (
         <Card key={index} data={item} />
       ))}
---
+++ src/app/explore/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import Desc from "../../components/description";
+import REQ from "../../components/requirements";
+
+export default function Details() {
+  return (
+    <>
+      <Desc />
+      <REQ />
+    </>
+  );
+}
